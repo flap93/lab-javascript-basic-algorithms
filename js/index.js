2bd4fb44c74da398ffee71a9e0d0024f870cd982
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2020-12-20 22:27:24 -0500

    iteration 3

--- js/index.js
+++ js/index.js
@@ -60,3 +60,30 @@ console.log(`The navigator\'s name is ${hacker2}`);
 
 
 // Iteration 3: Loops
+
+/*3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+
+3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?*/
+
+//3.1
+
+const name = 'john';
+
+for (let value of name) {
+  value = value.toUpperCase();
+  console.log(value);
+} /* ouput: JOHN */
+
+// 3.2
+
+let nameReverse = '';
+for (let i = name.length - 1; i >= 0; i--) {
+  nameReverse += name[i];
+}
+
+console.log(nameReverse); /* ouput: nhoj */ 
